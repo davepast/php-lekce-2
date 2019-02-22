ca0aad9f559e1f47871ec000ac9068f898ed475c
davepast <david.pastuszek@hotmail.com>
2019-02-22 21:14:14 +0100

    trojuhelnik obsah

--- pythagoras.php
+++ pythagoras.php
@@ -1,12 +1,17 @@
 <?php
 $a = 3;
 $b = 4;
-
-
-
+$strana = 6;
+$uhel = 60;
 
 $obdelnikObsah = $a * $b;
+$vyska = $strana * sin(deg2rad($uhel)); //v= a * sin uhlu (prevedeni deg -> rad, pak fce)
+$trojuhelnikObsah = round($vyska * $strana / 2); // round() podle pravidel
+?>
 
 
-
+<?php
 echo "Obsah obdelníku o stranách a: $a cm a b: $b cm je $obdelnikObsah,";
+echo '<br>';
+echo "Obsah rovnostranného trojúhelníku o straně a: $strana cm a úhlu: $uhel" . '°' . "je $trojuhelnikObsah cm";
+?>
